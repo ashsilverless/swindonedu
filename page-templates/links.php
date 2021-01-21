8dd4704889c5c21de1cb9e60990996418a3ce3fa
Ash - SIlverless <ash@silverless.co.uk>
2021-01-21 13:58:25 +0000

    DEV - adjust links page

--- page-templates/links.php
+++ page-templates/links.php
@@ -14,14 +14,17 @@ get_header();?>
 		</section>
 		<section>
 			<div class="general-links">
-			<?php if( have_rows('links') ):
-			while( have_rows('links') ): the_row(); ?>
-			<div class="general-links__item">
-				<h3 class="heading heading__6"><?php the_sub_field('title');?></h3>
-				<?php the_sub_field('description');?>
-				<a href="<?php the_sub_field('link');?>" target="_blank"><i class="fas fa-chevron-right"></i></a>
-			</div>
-			<?php endwhile; endif;?>
+			<?php if( have_rows('link_section') ):
+			while( have_rows('link_section') ): the_row(); ?>
+			<h3 class="heading heading__6"><?php the_sub_field('section_heading');?></h3>
+				<?php if( have_rows('links') ):
+				while( have_rows('links') ): the_row(); ?>
+					<div class="general-links__item">
+						<p><?php the_sub_field('link_desscription');?></p>
+						<a href="<?php the_sub_field('link_to_page');?><?php the_sub_field('link_to_document');?>" target="_blank"><i class="fas fa-chevron-right"></i></a>
+					</div>
+				<?php endwhile; endif;?>	
+			<?php endwhile; endif;?>	
 			</div>
 		</section>
 	</div>
