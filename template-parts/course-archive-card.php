28a7efc60b5cb0cdcf9b1a7d30278629cad9e1db
Ash - SIlverless <ash@silverless.co.uk>
2021-03-05 09:30:31 +0000

    DEV - lecturers on course card

--- template-parts/course-archive-card.php
+++ template-parts/course-archive-card.php
@@ -24,7 +24,9 @@ if ($current_tags){
 	<div class="summary">
 		<p><?php the_field('delivered_on');?></p>
 		<h2 class="heading heading__5"><?php the_title();?></h2>
-		<p><?php the_field('lecturer');?></p>
+		<?php while( have_rows('course_lecturers') ): the_row(); ?>
+		<p><?php the_sub_field('lecturer');?></p>
+		<?php endwhile;?>
 	</div>
 	<a href="<?php the_permalink();?>" class="book">
 		<span>
